106946c498dfdcfb219aa01e36ee47a8161bb67b
arsyabhrn <alfatehanarsya8998@gmail.com>
2022-02-24 21:06:58 +0700

    86. 88. Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe: Recipe[] = [
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1623691752472-a6d33855e5de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1623691752472-a6d33855e5de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')
   ];
@@ -15,6 +17,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
